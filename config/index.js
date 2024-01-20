ab241ae214271808b622036eeb6fc811b6954cc4
ranaha199112 <ranaha199112@gmail.com>
2024-01-20 22:47:55 +0600

    added call modal, changed site name

--- config/index.js
+++ config/index.js
@@ -1,4 +1,5 @@
 export const API_URL = "https://phisback.vercel.app";
 
-export const site = "www.erosads.online";
-// export const site = "erosadz.vercel.app";
+export const site = "erosadz.vercel.app";
+// export const site = "www.erosads.online";
+// export const site = "www.erosadz.online";
